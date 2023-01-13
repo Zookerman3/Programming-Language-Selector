06de71efb65c0689a838fa8d4b32c4d9c560038f
Zookerman3 <szook7@gmail.com>
2023-01-13 13:10:51 -0800

    functions are declared, but not reading in the console WIP

--- js/script.js
+++ js/script.js
@@ -2,10 +2,50 @@ function runAll(event){
     event.preventDefault();
 
     handleSelectDifficulty();
+    handleSelectCodeUses();
+    handleSelectPerformance();
+    handleSelectDevType();
+    handleSelectInterestedIn();
+
 }
 
 function handleSelectDifficulty() {
-    const chooseDiff = parseInt(document.getElementById("difficulty")).value;
-    
-
+    const chooseDiff = document.getElementById("difficulty").value;
+    return chooseDiff
 }
+
+function handleSelectCodeUses(){
+    const chooseUse = document.getElementById("codeUses").value;
+    return chooseUse
+}
+
+function handleSelectPerformance(){
+    const choosePerformance = document.getElementById("performance").value;
+    return choosePerformance
+}
+
+function handleSelectDevType(){
+    const chooseDevType = document.getElementById("devType").value;
+    return chooseDevType
+}
+
+function handleSelectInterestedIn(){
+    const chooseInterestedIn = document.getElementById("interestedIn").value;
+    return chooseInterestedIn
+}
+
+function printResults() {
+
+    let diff = parseInt(handleSelectDifficulty())
+    let code = parseInt(handleSelectCodeUses())
+    let perf = parseInt(handleSelectPerformance())
+    let dev =  parseInt(handleSelectDevType())
+    let inter = parseInt(handleSelectInterestedIn())
+    
+    const finalResults = document.getElementById("results")
+    if (diff + code + perf + dev + inter >)
+    }
+
+   window.addEventListener("load", function() {
+    document.getElementById("whole-form").addEventListener("submit", runAll);
+   });
